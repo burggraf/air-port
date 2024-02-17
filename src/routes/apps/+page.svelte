a4b8f9011cae2237c0e2b176fd569595c07d8c22
Mark Burggraf <markb@mantisbible.com>
2024-02-17 06:53:49 -0800

    projects -> apps

--- src/routes/apps/+page.svelte
+++ src/routes/apps/+page.svelte
@@ -1,25 +1,27 @@
 <script lang="ts">
 	import { goto } from '$app/navigation'
 	import IonPage from '$ionpage'
-	import { addOutline, constructOutline, ellipseSharp, eyeOutline } from 'ionicons/icons'
+	import { addOutline } from 'ionicons/icons'
 	import { pb, currentUser } from '$services/backend.service'
-	let projects: any = []
-	let project_instances: any = []
+	import type { AppsRecord, MachinesRecord } from '$models/pocketbase-types'
+	import { getRegionName, regions } from '$services/region.service'
+	import { toast } from '$services/toast'
+
+	let apps: AppsRecord[] = [];
+	let machines: MachinesRecord[] = [];
+
 	const ionViewWillEnter = async () => {
 		if (!$currentUser) {
 			goto('/');
 		}
-		const instance_records = await pb.collection('instance_view').getFullList({
-			sort: 'name,type,site_name,instance_status',
+		machines = await pb.collection('machines').getFullList({
+			// sort: 'name,type,site_name,instance_status',
 		})
-		project_instances = instance_records
-		const project_records = await pb.collection('projects').getFullList({
-			sort: 'name',
+		console.log('machines', machines)
+		apps = await pb.collection('apps').getFullList({
+			sort: 'title',
 		})
-		projects = project_records
-		if (projects.length === 0) {
-			// goto('/new-app')
-		}
+		console.log('apps', apps)
 		if ($currentUser && $currentUser.verified === false) {
 			pb.collection('users').subscribe($currentUser.id, function (e) {
 				if (e.record?.verified) {
@@ -33,27 +35,28 @@
 	const newApp = async () => {
 		goto('/new-app')
 	}
-	const getInstancesForProject = (project_id: string) => {
-		return project_instances.filter((instance: any) => {
-			return instance.project_id === project_id
+	const getMachinesForApp = (domain: string) => {
+		return machines.filter((machine: any) => {
+			return machine.Domain === domain
 		})
 	}
 	let filterValue = ''
-	const filterProjects = (e: any) => {
+	const filterApps = (e: any) => {
 
-		for (let i = 0; i < projects.length; i++) {
-			const project = projects[i]
-			projects[i].hidden = false
+		for (let i = 0; i < apps.length; i++) {
+			const app = apps[i]
+			app[i].hidden = false
 		}
 		const value = e.target.value.toLowerCase()
 		if (value !== '') {
-			for (let i = 0; i < projects.length; i++) {
-				const project = projects[i]
+			for (let i = 0; i < apps.length; i++) {
+				const app = apps[i]
 				if (
-					project.name.toLowerCase().indexOf(value) === -1 &&
-					project.domain.toLowerCase().indexOf(value) === -1
+					app.title && app.Domain &&
+					app.title.toLowerCase().indexOf(value) === -1 &&
+					app.Domain.toLowerCase().indexOf(value) === -1
 				) {
-					projects[i].hidden = true
+					app[i].hidden = true
 				}
 			}
 		}
@@ -66,7 +69,7 @@
 			<ion-buttons slot="start">
 				<ion-menu-button />
 			</ion-buttons>
-			<ion-title>air-port Projects</ion-title>
+			<ion-title>air-port apps</ion-title>
 			<ion-buttons slot="end">
 				{#if $currentUser?.verified === true}
 					<ion-button on:click={newApp}>
@@ -78,10 +81,10 @@
 	</ion-header>
 	<ion-content>
 		{#if $currentUser}
-		{#if projects.length > 4}
+		{#if apps.length > 4}
 		<ion-searchbar value={filterValue} 
 			debounce={500} 
-			on:ionInput={filterProjects}
+			on:ionInput={filterApps}
 			style="margin-left: 10px;padding-right: 30px;" 
 			placeholder="search for project"></ion-searchbar>
 		{/if}
@@ -92,24 +95,24 @@
 					<ion-card-title>Verify Your Email</ion-card-title>
 				</ion-card-header>
 				<ion-card-content>
-					You need to verify your email before you can create a project. Please
+					You need to verify your email before you can create an app. Please
 					check your email for a verification link.
 				</ion-card-content>
 			</ion-card>
 		</div>
 		{/if}
-		{#if projects.length === 0 && $currentUser.verified === true}
+		{#if apps.length === 0 && $currentUser.verified === true}
 		<div class="width-500">
 			<ion-card>
 				<ion-card-header>
-					<ion-card-title>No Projects Yet</ion-card-title>
+					<ion-card-title>No Apps Yet</ion-card-title>
 				</ion-card-header>
 				<ion-card-content>
-					Looks like you don't have any projects yet. Click the button below to
-					start a new project.
+					Looks like you don't have any apps yet. Click the button below to
+					create a new app.
 					<ion-button expand="block" on:click={newApp}>
 						<ion-icon slot="icon-only" icon={addOutline} />
-						Create New Project
+						Create New App
 					</ion-button>
 				</ion-card-content>
 			</ion-card>
@@ -117,47 +120,46 @@
 		{:else}
 		<ion-list>
 			<div class="grid-container">
-				{#each projects as project}
-					<ion-card style="max-width: 400px; display: {project.hidden?'none':'block'};">
+				{#each apps as app}
+					<ion-card style="max-width: 400px; display: {app.hidden?'none':'block'};">
 						<ion-card-header style="cursor:pointer;"
 						on:click|stopPropagation={() => {
-							goto(`/project/${project.id}`)
+							goto(`/app/${app.id}`)
 						}}
 						>
-							<ion-card-title>{project.name}</ion-card-title>
+							<ion-card-title>{app.title}</ion-card-title>
 							<ion-card-subtitle>
 								<ion-grid><ion-row>
-									<ion-col>{project.domain}</ion-col>
-									<ion-col class="ion-text-right">{project.type}</ion-col>
+									<ion-col>{app.Domain}</ion-col>
+									<ion-col class="ion-text-right">{app.type}</ion-col>
 								</ion-row></ion-grid>								
 							</ion-card-subtitle>
 						</ion-card-header>
 
 						<ion-card-content>
 							<ion-list>
-								{#each getInstancesForProject(project.id) as instance}
+								{#each getMachinesForApp(app.Domain) as machine}
 									<ion-item
 										style="cursor:pointer;--padding-start:0px;--inner-padding-end: 0px;"
 										lines="full"
 										on:click={() => {
-											goto(`/instance/${instance.id}`)
+											goto(`/machine/${machine.id}`)
 										}}
 									>
-										{instance.site_name}
-										{instance.type}<br />
-										{instance.domain}.{instance.site_domain}
+										{getRegionName(machine.region || "")}
 
 										<ion-button
 										slot="end"
 										size="small"
 										fill="solid"
-										color={instance.instance_status === 'online' ? 'success' : instance.instance_status === 'offline' ? 'danger' : 'warning'}
+										color={machine.state === 'started' ? 'success' : 'danger'}
 										on:click|stopPropagation={() => {
+											toast('not implemented yet', 'danger')
 											// launch in another windows
-											window.open(
-												`https://${instance.domain}.${instance.site_domain}/`,
-												'_blank'
-											)
+											// window.open(
+											// 	`https://${instance.domain}.${instance.site_domain}/`,
+											// 	'_blank'
+											// )
 										}}
 									>
 										<ion-icon slot="icon-only" src="/launch.svg" />
@@ -170,10 +172,11 @@
 											fill="clear"
 											on:click|stopPropagation={() => {
 												// launch in another windows
-												window.open(
-													`https://${instance.domain}.${instance.site_domain}/_/`,
-													'_blank'
-												)
+												toast('not implemented yet', 'danger')
+												// window.open(
+												// 	`https://${instance.domain}.${instance.site_domain}/_/`,
+												// 	'_blank'
+												// )
 											}}
 										>
 											<ion-icon slot="icon-only" src="/pb.svg" />
